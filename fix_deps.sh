9983ab54a3daf70f68892199652850aef86c8ff4
Shiviatrix <73530491+Shiviatrix@users.noreply.github.com>
2026-07-21 03:48:26 +0530

    chore: clean up gitignore after moving scratch files

--- fix_deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Update dependencies in Cargo.toml files inside crates
-find crates -name "Cargo.toml" -type f -exec sed -i '' -e 's/eng-/vinglish-/g' {} +
-
-# Test it
-cargo check --workspace
--- fix_ext.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-sed -i '' -e 's/"eng"/"ving"/g' crates/vinglish-cli/src/main.rs
-# But wait! I added `"eng" || ext == "c" ...` in cmd_benchmark! I should change `"eng"` to `"ving"` there too!
-# Let's just sed it all, but let's review:
-
-cargo check --workspace
